6ada1902f9dbe3f9f83aec05e9a1582f8ffd07a4
rghidei <77600670+rghidei@users.noreply.github.com>
2021-02-12 14:59:12 -0500

    1stComment

---
+++ CodeWars/multiply.js
@@ -0,0 +1,6 @@
+/*
+This code does not execute properly. Try to figure out why.
+*/
+multiply = function (a, b) {
+  return a * b;
+}
---
+++ CodeWars/numberToString.js
@@ -0,0 +1,8 @@
+/*
+The code gives an error!
+
+a = 123.toString
+Fix it!
+*/
+var a = "123";
+a.toString();

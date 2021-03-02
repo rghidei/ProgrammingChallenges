68780516c8c32f2bf8835efbd53cdd35e79c7797
Rodas Ghidei <rodasghidei@yahoo.comm>
2021-03-02 01:05:05 -0500

    add js file

---
+++ CodeWars/exclamationMark2.js
@@ -0,0 +1,20 @@
+//Description:
+//Remove all exclamation marks from the end of sentence.
+//Examples:
+//remove("Hi!") === "Hi"
+//remove("Hi!!!") === "Hi"
+//remove("!Hi") === "!Hi"
+//remove("Hi! Hi!") === "Hi! Hi"
+//remove("Hi") === "Hi"
+
+function remove(s){
+  let result = s
+
+  for(let i = 0; i <= s.length;i++)
+  if(result[result.length - 1] === "!"){
+
+   result =  result.slice(0, -1)
+   continue
+  }
+  return result
+}

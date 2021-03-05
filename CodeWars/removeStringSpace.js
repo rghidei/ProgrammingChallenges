5b97a47051949669bb394b72314a22482e7354c9
Rodas Ghidei <rodasghidei@yahoo.com>
2021-03-04 22:59:57 -0500

    add codeWar file

---
+++ CodeWars/removeStringSpace.js
@@ -0,0 +1,8 @@
+// Description:
+// Simple, remove the spaces from the string,
+// then return the resultant string.
+function noSpace(x){
+  const removeSpace = x.replace(/\s+/g, '')
+  return removeSpace
+
+}

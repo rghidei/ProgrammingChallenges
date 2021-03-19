e320e5f7e545433f6cf9df2473f1454215f4d86f
rghidei <rodasghidei@yahoo.com>
2021-03-18 23:28:19 -0400

    add Codewar File

---
+++ CodeWars/reversedWords.js
@@ -0,0 +1,9 @@
+// Description:
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+function reverseWords(str){
+  str = str.split(' ')
+  str = str.reverse()
+  str = str.join(' ')
+  return str
+}

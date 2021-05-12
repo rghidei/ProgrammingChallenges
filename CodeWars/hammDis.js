4d108fccd3d6dcff9c678ceb02b2c6fd09dcf6a0
Rodas Ghidei <rodasghidei@yahoo.com>
2021-05-11 23:33:39 -0400

    upload file

---
+++ CodeWars/hammDis.js
@@ -0,0 +1,20 @@
+/* CHALLENGE
+Given two strings of equal length, calculate and return the the hamming distance.
+E.g hammingDistance('rover', 'river') // should return 1
+*/
+
+
+
+
+function hammingDistance(stringA, stringB) {
+    // Code goes here
+  let splitA = stringA.split('')
+  let splitB = stringB.split('')
+  let counter = 0
+  for(let i = 0; i < splitA.length; i++){
+    if(splitA[i] !== splitB[i]){
+        counter++
+    }
+  }
+  return counter
+}

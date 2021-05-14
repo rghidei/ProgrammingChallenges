5f8c1c5a2ecb570604d31fa09d8c37d8c3ba3daa
Rodas Ghidei <rodasghidei@yahoo.com>
2021-05-13 23:36:06 -0400

    upload file

---
+++ CodeWars/capSen.js
@@ -0,0 +1,31 @@
+/* CHALLENGE
+Given a sentence containing two or more words,
+return the equivalent of the sentence when capitalised. E.g
+  capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!'
+*/
+
+
+
+
+ function capSentence(text) {
+
+  }
+
+module.exports = capSentence
+
+
+//
+// function capSentence(text) {
+//    // Code goes here
+// }
+//
+//
+//
+// module.exports = capSentence
+// function capSentence(text) {
+//      let wordsInTheArray = text.toLowerCase().split(' ')
+//      let capsInTheArray = wordsInTheArray.map(word=>{
+//          return word[0].toUpperCase() + word.slice(1)
+//      })
+//      return capsInTheArray.join(' ')
+//  }

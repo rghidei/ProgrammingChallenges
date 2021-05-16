151a6b977e4e3d2bd09ad9f1d11e0330c0ff0dd4
Rodas Ghidei <rodasghidei@yahoo.com>
2021-05-16 00:05:25 -0400

    upload file

---
+++ CodeWars/algomaxrec.js
@@ -0,0 +1,27 @@
+/* CHALLENGE
+Given a string of text, find and return the most recurring character.
+e.g maxRecurringChar('aabacada') // will return 'a'
+*/
+
+
+
+function maxRecurringChar(str) {
+    // Code goes here
+    const charMap = {};
+  let max = 0;
+  let maxChar = "";
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}

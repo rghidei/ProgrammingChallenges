2a205495c9e7c66a85c81ebc3a2b82b07697a6bc
Rodas Ghidei <rodasghidei@yahoo.com>
2021-06-02 19:14:40 -0400

    update

--- CodeWars/algochunkarr.js
+++ CodeWars/algochunkarr.js
@@ -3,6 +3,7 @@
  their elements into one array without any repetition.
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
+///
 
 function chunkArray(array, size) {
     console.log('===============================================')

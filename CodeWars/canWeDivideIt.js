fd4ce4d08b6cda1dbbbe58f14ce198c6dad17906
Rodas Ghidei <rodasghidei@yahoo.com>
2021-06-05 23:22:47 -0400

    add file

--- CodeWars/canWeDivideIt.js
+++ CodeWars/canWeDivideIt.js
@@ -2,7 +2,6 @@
 //Your task is to create functionisDivideBy (or is_divide_by) to check if an integer number is divisible by each out of two arguments.
 
 function isDivideBy(number, a, b) {
-  // good luck
   if(number % a === 0 && number % b === 0){
     return true
   } else{
@@ -10,4 +9,3 @@ function isDivideBy(number, a, b) {
   }
 }
 console.log(isDivideBy(-12, 2, -5))
-// 

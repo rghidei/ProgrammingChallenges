83cde49ba982197198d4aa4a1ce474cf7a145b4e
Rodas Ghidei <rodasghidei@yahoo.com>
2021-06-07 16:30:51 -0400

    add codewar challenge

---
+++ CodeWars/sumTheStrings.js
@@ -0,0 +1,28 @@
+// Create a function that takes 2 nonnegative integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39
+
+// p: strings
+// r: return the sum of the strings
+// p:
+// -convert the strings into an integer
+// -add the integers to get their sum
+// -convert back into a string 
+
+function sumStr(a,b) {
+  if(a === '' && b === ''){
+    a = 0
+    b = 0
+  } else if(a === '') {
+    a = 0
+  } else if (b === ''){
+    b = 0
+  }
+  let conA = parseInt(a, 10)
+  let conB = parseInt(b, 10)
+  let sum = conA + conB
+  return sum.toString()
+}

83c0bdb06df34beb76e09e07a518d65eec73629e
Rodas Ghidei <rodasghidei@yahoo.com>
2021-06-06 23:23:06 -0400

    add file

--- CodeWars/filterOutTheGeese.js
+++ CodeWars/filterOutTheGeese.js
@@ -15,4 +15,4 @@ function gooseFilter (birds) {
   var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
   return birds.filter(item => geese.includes(item) === false)
   // return an array containing all of the strings in the input array except those that match strings in geese
-}
+};

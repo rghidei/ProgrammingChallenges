33db17ec5728f2a7b005f1b57c984e289891f169
Rodas Ghidei <rodasghidei@yahoo.com>
2021-06-28 23:13:40 -0400

    add file

--- CodeWars/algopalchc.js
+++ CodeWars/algopalchc.js
@@ -11,14 +11,13 @@ e.g palindromeChecker('racecar') // will return true
 // }
 function palindromeChecker(text) {
   return text.split("").reverse().join("") === text ? true: false;
-    // Code goes here
-    // let palindromeText = text.split("").reverse().join("");
-    //
-    // if(text == palindromeText){
-    //     return true
-    // }
-    // else {
-    //     return false
-    // }
+    let palindromeText = text.split("").reverse().join("");
+
+    if(text == palindromeText){
+        return true
+    }
+    else {
+        return false
+    }
 }
 // const palindromeChecker = (text) => text.split('').reverse().join('') === text ? true : false

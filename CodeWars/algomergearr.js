02e93be6ee8eaf9781427dc4fcbbd2614893d345
Rodas Ghidei <rodasghidei@yahoo.com>
2021-07-28 00:01:32 -0400

    updating files

--- CodeWars/algomergearr.js
+++ CodeWars/algomergearr.js
@@ -15,17 +15,17 @@ return [...new Set([...text])]
 // console.log(mergeArrays([1,2,3,3,3], [1,4,5,2]))
 
 //longer version
-// function mergeArrays(arr1, arr2) {
-//
-// let text = arr1.concat(arr2)
-// // console.log(text)
-// text = text.sort()
-// // console.log(text)
-// text = [...new Set([...text])]
-// // console.log(text)
-//   return text
-// }
-// console.log(mergeArrays([1,2,3,3,3], [1,4,5,2]))
+function mergeArrays(arr1, arr2) {
+
+let text = arr1.concat(arr2)
+// console.log(text)
+text = text.sort()
+// console.log(text)
+text = [...new Set([...text])]
+// console.log(text)
+  return text
+}
+console.log(mergeArrays([1,2,3,3,3], [1,4,5,2]))
 
 
 // function mergeArrays(...arrays) {

581969fa5c540d4c29c6c33e172bf7ef8f3ead6a
Rodas Ghidei <rodasghidei@yahoo.com>
2021-07-28 23:52:16 -0400

    update files

--- CodeWars/arrayPlusArray.js
+++ CodeWars/arrayPlusArray.js
@@ -8,6 +8,5 @@ function arrayPlusArray(arr1, arr2) {
   let result = arr1.reduce((sum, current) => sum + current, 0);
   let result2 = arr2.reduce((sum, current) => sum + current, 0);
 
-  return result + result2; //something went wrong
+  return result + result2;
 }
-//

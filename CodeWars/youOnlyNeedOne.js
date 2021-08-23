495521393ab2b4e092e650ecb84d28f9c9f92c10
Rodas Ghidei <rodasghidei@yahoo.com>
2021-08-23 11:00:51 -0400

    update file

--- CodeWars/youOnlyNeedOne.js
+++ CodeWars/youOnlyNeedOne.js
@@ -7,3 +7,14 @@
 
 
 Test Cases:
+check([66, 101], 66), true);
+check([78, 117, 110, 99, 104, 117, 107, 115], 8), false);
+check([101, 45, 75, 105, 99, 107], 107), true);
+check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+check(['t', 'e', 's', 't'], 'e'), true);
+check(['what', 'a', 'great', 'kata'], 'kat'), false);
+check([66, ,'codewars', 11, 'alex loves pushups'], 'alex loves pushups'), true);
+check(['come', 'on', 110, '2500', 10, '!', 7, 15], 'Come'), false);
+check(['when\'s', 'the', 'next', 'Katathon?', 9, 7], 'Katathon?'), true);
+check([8, 7, 5, 'bored', 'of', 'writing', 'tests', 115], 45), false);
+check(['anyone', 'want', 'to', 'hire', 'me?'], 'me?'), true);

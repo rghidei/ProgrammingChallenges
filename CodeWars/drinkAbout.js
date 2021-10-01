3c925db38555834978dea8b8dc926a844b32fd0b
Rodas Ghidei <rodasghidei@yahoo.com>
2021-09-30 23:56:35 -0400

    update file

--- CodeWars/drinkAbout.js
+++ CodeWars/drinkAbout.js
@@ -44,3 +44,7 @@ console.log(peopleWithAgeDrink(21))
 console.log(peopleWithAgeDrink(30))
 
 // Best Solution:
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"

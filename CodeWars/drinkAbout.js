0628e99c6f6fb8f5275f00a361c01babbd4d88d2
Rodas Ghidei <rodasghidei@yahoo.com>
2021-10-14 23:46:37 -0400

    update file

---
+++ CodeWars/drinkAbout.js
@@ -0,0 +1,14 @@
+Description:
+Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+Examples: (Input --> Output)

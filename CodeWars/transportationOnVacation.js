c44129847166c28f57e24b55fa9f985266901419
Rodas Ghidei <rodasghidei@yahoo.com>
2021-11-08 21:43:58 -0500

    update file

--- CodeWars/transportationOnVacation.js
+++ CodeWars/transportationOnVacation.js
@@ -6,3 +6,9 @@
 // Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 //
 // Write a code that gives out the total amount for different days(d).
+
+Test Cases:
+(rentalCarCost(3), 100);
+(rentalCarCost(4), 140);
+(rentalCarCost(5), 180);
+(rentalCarCost(6), 220);

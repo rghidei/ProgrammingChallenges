09dbe7f1ebdc85f15ed4747c1e4048b27e2857c0
Rodas Ghidei <rodasghidei@yahoo.com>
2021-11-17 20:48:18 -0500

    update file

--- CodeWars/switchItUp.js
+++ CodeWars/switchItUp.js
@@ -7,3 +7,29 @@
 // Output :: "One".
 
 // My Solution:
+function switchItUp(number){
+//Write your own Code!
+   if (number === 0) {
+     return "Zero";
+   } else if (number === 1) {
+     return "One";
+   } else if (number === 2) {
+     return "Two";
+   } else if (number === 3) {
+     return "Three";
+   } else if (number === 4) {
+     return "Four";
+   } else if (number === 5) {
+     return "Five";
+   } else if (number === 6) {
+     return "Six";
+   } else if (number === 7) {
+     return "Seven";
+   } else if (number === 8) {
+     return "Eight";
+   } else {
+     return "Nine";
+   }
+}
+
+console.log(switchItUp(5))
